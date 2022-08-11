bd50e1d5593ef2f828944182195e1295150e63fb
maikelordaz <maikelordaz@gmail.com>
2022-08-10 21:55:03 -0400

    Update 02-deploy-random-ipfs-nft.js

---
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -0,0 +1,11 @@
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+const { developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+}
+
+module.exports.tags = ["all", "basicnft", "main"]
